6d3a9415893a1b8ce5cda7d3e25ec40d54797110
Admin <yeuthuongvoivangdiqua@gmail.com>
2024-09-02 12:11:22 +0700

    init project

---
+++ src/main/java/com/example/BE_FACE_NOTE_BOOK_APP_MAVEN/BeFaceNoteBookAppMavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.BE_FACE_NOTE_BOOK_APP_MAVEN;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeFaceNoteBookAppMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeFaceNoteBookAppMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BE_FACE_NOTE_BOOK_APP_MAVEN/BeFaceNoteBookAppMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BE_FACE_NOTE_BOOK_APP_MAVEN;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeFaceNoteBookAppMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
